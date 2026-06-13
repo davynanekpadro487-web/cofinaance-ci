9e8339259051060691a67e36228053aafa83abe8
unknown <davynanekpadro487@gmail.com>
2026-06-13 19:29:01 +0000

    Fix assurance prix et types corriges

--- cofinci/urls.py
+++ cofinci/urls.py
@@ -20,4 +20,22 @@ urlpatterns = [
     path('chat/', TemplateView.as_view(
         template_name='support/chat.html'
     ), name='chat'),
+    path('', TemplateView.as_view(
+        template_name='frontend/login.html'), 
+        name='login'),
+    path('dashboard/', TemplateView.as_view(
+        template_name='frontend/dashboard.html'), 
+        name='dashboard'),
+    path('credits/', TemplateView.as_view(
+        template_name='frontend/credits.html'), 
+        name='credits'),
+    path('assurance/', TemplateView.as_view(
+        template_name='frontend/assurance.html'), 
+        name='assurance_frontend'),
+    path('notifications/', TemplateView.as_view(
+        template_name='frontend/notifications.html'), 
+        name='notifications_frontend'),
+    path('profil/', TemplateView.as_view(
+        template_name='frontend/profil.html'), 
+        name='profil'),
 ]
